d7a426a9be48924e996cde6e4cd332aedccc6d8e
ppsp02 <ppsp02@naver.com>
2024-03-14 22:57:24 +0900

    Revert "커밋완료"
    This reverts commit 6c051834120661a262a892397619c5a23df6770b.

--- src/main/java/com/Spring/Graduate/Project/Seoul/Controller/RankController.java
+++ src/main/java/com/Spring/Graduate/Project/Seoul/Controller/RankController.java
@@ -1,15 +1,12 @@
 package com.Spring.Graduate.Project.Seoul.Controller;
 
 import com.Spring.Graduate.Project.Seoul.DTO.TeamDTO;
-import com.Spring.Graduate.Project.Seoul.DTO.TeambDTO;
 import com.Spring.Graduate.Project.Seoul.Service.ScrapingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -28,9 +25,6 @@ public class RankController {
 
         model.addAttribute("teamDataList", teamDataList);
 
-        List<TeambDTO> teambDataList = ScrapingService.scrapebRank();
-
-        model.addAttribute("teambDataList", teambDataList);
 
         return "Mainindex";
 
